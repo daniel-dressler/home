2a23cd0629c1db0cc629f79e77f38f2b47b111b4
Daniel Dressler <danieru.dressler@gmail.com>
2013-04-02 16:15:00 -0600

    Add color to awesome theme

--- .config/awesome/themes.lua
+++ .config/awesome/themes.lua
@@ -4,22 +4,22 @@
 
 theme = {}
 
-theme.font          = "sans 8"
+theme.font          = "ubuntu-mono 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#d5e286"
+theme.bg_focus      = "#b6d884"
 theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
+theme.bg_minimize   = "#8fcd9e"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+theme.fg_normal     = "#444444"
+theme.fg_focus      = "#333333"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = 1
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_normal = "#333333"
+theme.border_focus  = theme.bg_focus
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
@@ -72,7 +72,8 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper_cmd = {"feh --bg-tile /home/danieru/.config/awesome/rose.png"}
+-- theme.wallpaper_cmd = {"feh --bg-tile /home/danieru/.config/awesome/rose.png"}
+theme.wallpaper_cmd = {"xsetroot -solid '#333333'"}
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
